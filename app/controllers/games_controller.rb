26cede8f454f00f82e35629fb91e54813ea0cd7f
mac <mac@MacBook-Pro-de-mac-4.local>
2019-04-08 16:07:55 +0200

    final

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,33 @@
 class GamesController < ApplicationController
  
+  QUESTIONS = [
+   "Does it have clear eyes?",
+   "Is this a male?",
+   "Is he vegan?",
+   "Is he from Aquitaine?"
+  ]
 
   def create
   	#créer le tableau de réponses vide
-  	redirect_to edit_game_path(1), method: :get
+  	redirect_to edit_game_path(1, 0), method: :get
   end
  
   def edit
-  end
+     @question_id = params["question_id"].to_i
+    @question = QUESTIONS[@question_id]
+ end
 
   def update
-  	redirect_to game_path(1), method: :get
+    @question_id = params["question_id"].to_i
+    if @question_id < QUESTIONS.size
+      redirect_to edit_game_path(id: 1, question_id: @question_id), method: :get
+    else
+      redirect_to game_path(id: 1), method: :get
+    end
   end
 
   def show
+    @question_id = QUESTIONS
   end
 
   def home
